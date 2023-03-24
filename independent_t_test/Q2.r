3c156cf1246cc2e8d45425ee4414c7fb1786f97e
Lexin Chen <110304515+lexin-chen@users.noreply.github.com>
2023-03-24 13:36:04 -0400

    Update Q2.r

--- independent_t_test/Q2.r
+++ independent_t_test/Q2.r
@@ -5,7 +5,7 @@
 library(tidyverse)
 library(readr)
 
-df <- read_csv("E:\\midterm\\Question2.csv")
+df <- read_csv("Question2.csv")
 M1_df <- subset(df, Method == "M1")
 M2_df <- subset(df, Method == "M2")
 
@@ -13,12 +13,12 @@ M2_df <- subset(df, Method == "M2")
 leveneTest(M1_df$Tin, M2_df$Tin)
 
 #2c perform independent t-test, var.equal is true because 2a
-t.test(M1_df$Tin, M2_df$Tin, var.equal=TRUE)
+t.test(M1_df$Tin, M2_df$Tin, var.equal = TRUE)
 
 # 2d mathematical method is shapiro-wilk.
 # Shapiro-wilk test is used to test if the data is normal distribution
 shapiro.test(M1_df$Tin)
 shapiro.test(M2_df$Tin)
-qqplot(qqnorm(M1_df$Tin, main="QQ plot for M1"))
-qqplot(qqnorm(M2_df$Tin, main="QQ plot for M2"))
-par(mfrow=c(1,2))
+qqplot(qqnorm(M1_df$Tin, main = "QQ plot for M1"))
+qqplot(qqnorm(M2_df$Tin, main = "QQ plot for M2"))
+par(mfrow = c(1, 2))
