cc73f4fe25ef9e6a98f41750e6e5a4cc1abe3f57
Lexin Chen <110304515+lexin-chen@users.noreply.github.com>
2023-03-24 13:36:59 -0400

    Update Q3.r

--- paired_t_test/Q3.r
+++ paired_t_test/Q3.r
@@ -5,11 +5,11 @@
 
 library(tidyverse)
 library(readr)
-df <- read_csv("E:\\midterm\\Question3.csv")
+df <- read_csv("Question3.csv")
 
 # 3a shapiro-wilk test to see if the data is normal distribution
 shapiro.test(df$UV)
 shapiro.test(df$NearIR)
 
 # 3b Finding the degrees of freedom and the p-value.
-t.test(df$UV, df$NearIR, paired=TRUE)
+t.test(df$UV, df$NearIR, paired = TRUE)
