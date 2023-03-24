f9802e8cc2d159efc8fce382e1bea99eb57e29be
Lexin Chen <110304515+lexin-chen@users.noreply.github.com>
2023-03-24 13:08:14 -0400

    Add files via upload

---
+++ paired_t_test/Q3.r
@@ -0,0 +1,15 @@
+# In a series of paired experiments,
+# 10 batches of samples were split between two measurement methods,
+# one analyzed using UV and one using near IR.
+# Is there significant difference between the two methods “Question3.csv”.
+
+library(tidyverse)
+library(readr)
+df <- read_csv("E:\\midterm\\Question3.csv")
+
+# 3a shapiro-wilk test to see if the data is normal distribution
+shapiro.test(df$UV)
+shapiro.test(df$NearIR)
+
+# 3b Finding the degrees of freedom and the p-value.
+t.test(df$UV, df$NearIR, paired=TRUE)
